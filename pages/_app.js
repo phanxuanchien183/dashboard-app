82d19c945a37a40f7f059f54ec36045d2eef97c9
phanxuanchien183 <phanxuanchien183@gmail.com>
2024-12-04 14:42:09 +0700

    create table and export excel

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import 'antd/dist/antd.css'; // Import CSS của Ant Design
-import '../styles/globals.css'; // Import CSS của Tailwind
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;

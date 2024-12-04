cd232102a98417ff675b1d2cd60252c5ad8b590e
phanxuanchien183 <phanxuanchien183@gmail.com>
2024-12-04 10:57:48 +0700

    Create new source

---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-center mt-10">Trang chủ</h1>
+    </div>
+  );
+};
+
+export default Home;
